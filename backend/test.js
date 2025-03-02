4336c80c9a743c4bbba19af3d26db85615dc1426
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-03-02 13:28:08 +0200

    new log message

--- backend/test.js
+++ backend/test.js
@@ -1,16 +1,16 @@
 function s(req, res) {
-    console.log(req);
-    console.log(res);
+  console.log(req);
+  console.log(res);
 }
 
 function createServer(s) {
-    const req= {req: 'req'}
-    const res = {res: 'res'}
+  const req = { req: "req" };
+  const res = { res: "res" };
 
-    s(req, res)
-    console.log(s);
-    
-    return ''
+  s(req, res);
+  console.log("YOU DOG");
+
+  return "";
 }
 
-createServer(s)
+createServer(s);
